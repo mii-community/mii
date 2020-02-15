55e837e760e9e1431aee45f684d6e5e6e9da16f1
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-15 16:11:46 +0900

    修正たくさん

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,14 @@
 import discord
-
 client = discord.Client()
 
-# 起動時に動作する処理
+# 起動通知
 @client.event
 async def on_ready():
     channel = client.get_channel(678041133179469834)
     await channel.send('起動しました。')
 
-# アカウント登録の処理
+# アカウント登録
+# 受付チャンネルの指定
 register_channel_id = "625220520698183690"
 @client.event
 async def on_message(message):
@@ -17,19 +17,21 @@ async def on_message(message):
         return
     # コマンド処理
     if message.content == "!register":
-        # コマンドを受け付けるチャンネルを指定
-        if message.channel != register_channel_id:
+        # コマンドを受け付けるチャンネルでの処理
+        if message.channel == register_channel_id:
+            # 参加者通知のチャンネルを指定
+            channel = client.get_channel(678041133179469834)
+            # メンバー数を再計算
+            user_count = sum(1 for member in channel.members if not member.bot)
+            # 参加者通知へメッセージ送信
+            await message.channel.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
+            # ダイレクトメッセージへのメッセージ送信
+            dm = await message.author.create_dm()
+            await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
+            # コマンドを受け付けないチャンネルでの処理
+        else:
             await message.send("ここでは実行できません。")
             return
-        # 参加者通知のチャンネルを指定
-        channel = client.get_channel(678041133179469834)
-        # メンバー数を再計算
-        user_count = sum(1 for member in channel.members if not member.bot)
-        # 参加者通知へメッセージ送信
-        await message.channel.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
-        # ダイレクトメッセージへのメッセージ送信
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
         
 # Botの起動とDiscordサーバーへの接続
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
