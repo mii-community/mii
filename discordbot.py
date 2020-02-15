f6d970d5293c9bc4213a7893c8b8a5cfac278078
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-15 17:30:34 +0900

    commit

--- discordbot.py
+++ discordbot.py
@@ -13,13 +13,13 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot: return
     if message.content == "!register":
-        join = client.get_channel(678041133179469834)
-        user_count = sum(1 for member in join.members if not member.bot)
-        # 参加者通知へメッセージ送信
-        await join.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
-        # ダイレクトメッセージを作成し、チュートリアルを送信
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
-        
+        if message.channel.id == 678136433512284208:
+            join = client.get_channel(678041133179469834)
+            user_count = sum(1 for member in join.members if not member.bot)
+            await join.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
+            dm = await message.author.create_dm()
+            await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
+        else: await message.channel.send("ここでは実行できません")
+
 # Botの起動とDiscordサーバーへの接続処理部
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
