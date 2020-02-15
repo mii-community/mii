8aeafd2c327df18a9aac538f76916d7ac34384f3
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-15 14:56:49 +0900

    testtt

--- discordbot.py
+++ discordbot.py
@@ -8,16 +8,18 @@ async def on_ready():
     channel = client.get_channel(678041133179469834)
     await channel.send('起動しました。')
 
-# メッセージ受信時に動作する処理
+# アカウント登録の処理
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
+    # BOTのメッセージに反応させない
+    if message.author == client.user:
         return
-    # 「/register」と発言したら「はじめまして」が返る処理
-    if message.content == "/message":
+    # コマンド処理
+    if message.content == "!register":
         channel = client.get_channel(678041133179469834)
-        await channel.send("メッセージです。")
+        user_count = sum(1 for member in chnannel.members if not member.bot)
+        message = f"{message.author.name}が参加しました。¥n{user_count}人目の参加者です。"
+        await message.channel.send("")
 
 # Botの起動とDiscordサーバーへの接続
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
