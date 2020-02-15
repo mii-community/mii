8fc1c7d32786d727805afc743c86bf7b060bc735
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-15 15:36:55 +0900

    アカウント登録の処理 修正
    ・特定のチャンネル以外で実行させないようにしました。
    ・ダイレクトメッセージのチャンネルをID指定にしました。

--- discordbot.py
+++ discordbot.py
@@ -9,12 +9,17 @@ async def on_ready():
     await channel.send('起動しました。')
 
 # アカウント登録の処理
+register_channel_id = "625220520698183690"
 @client.event
 async def on_message(message):
     # BOTのメッセージに反応させない
     if message.author == client.user:
         return
-
+    # 別のチャンネルで実行させない
+    if message.channel != register_channel_id:
+        await message.send("ここでは実行できません。")
+        return
+    if message.channel == client.get_channel(625220520698183690)
     # コマンド処理
     if message.content == "!register":
         # チャンネルを指定
@@ -25,7 +30,7 @@ async def on_message(message):
         await message.channel.send(f"{message.author.name}が参加しました。\n{user_count}人目の参加者です。")
         # ダイレクトメッセージへのメッセージ送信
         dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: #自己紹介 にて自己紹介をしましょう！\nstep2: #入退室パネル から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の #information が閲覧できるようになりました。")
+        await clean_content.dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
 
 # Botの起動とDiscordサーバーへの接続
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
