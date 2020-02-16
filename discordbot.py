923d2e6ffbb657b444c26b34f27e546dd14a7f4e
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-16 16:32:02 +0900

    ピン解除機能実装

--- discordbot.py
+++ discordbot.py
@@ -44,5 +44,27 @@ async def on_raw_reaction_add(payload):
                 await message.pin()
                 await channel.send(f"{user.name}がメッセージをピン留めしました。")
 
+# リアクション解除時の処理一覧
+@client.event
+async def on_raw_reaction_remove(payload):
+    # ピン解除処理部
+    if payload.emoji.name == '📌':
+        user = client.get_user(payload.user_id)
+        if user.bot: return
+        else:
+            channel = client.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            if message.pinned == 1:
+                reaction = discord.utils.get(message.reactions, emoji=payload.emoji.name)
+                # reaction単体だと比較できなかったためandでcountも追加
+                # 修正候補ここから
+                if reaction and reaction.count == 1: return
+                # ここまで
+                else:
+                    await message.unpin()
+                    await channel.send("リアクションがゼロになったため、ピン留めが解除されました。")
+                    embed = discord.Embed(title=f"送信者:{message.author}",description=f"メッセージ内容:{message.content}",color=0xff0000)
+                    await channel.send(embed=embed)
+
 # Botの起動とDiscordサーバーへの接続処理部
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
