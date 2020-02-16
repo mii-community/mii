21942f6c6efc7b7c47e566d69519ad217e6f6142
fe-amazu <fe-amazu@users.noreply.github.com>
2020-02-16 11:48:43 +0900

    ピン, アンピン機能調整

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author.bot: return
-    # アカウント登録機能処理部
+    # アカウント登録処理部
     if message.content == "!register":
         if message.channel.id == 653111096747491328:
             role = discord.utils.get(message.guild.roles, name="member")
@@ -23,7 +23,7 @@ async def on_message(message):
             dm = await message.author.create_dm()
             await dm.send(f"{message.author.mention} アカウントが登録されました。\nまず何をすればいいかわからない方へ▽\nstep1: <#655311853844430858> にて自己紹介をしましょう！\nstep2: <#653919145729064970> から各サーバーに入室してください！\n【Tips】スパム防止のため #welcome と #register は非表示になりました。そして #welcome の上位互換の <#661167351412162580> が閲覧できるようになりました。")
         else: await message.channel.send("ここでは実行できません。")
-    # サーバーアンケート機能処理部
+    # サーバーアンケート処理部
     if message.channel.id == 660392800399130633:
         sansei = '<:sansei:660392552528347157>'
         hantai = '<:hantai:660392595159121959>'
@@ -33,7 +33,7 @@ async def on_message(message):
 # リアクション追加時の処理一覧
 @client.event
 async def on_raw_reaction_add(payload):
-    #ピン留め機能処理部
+    #ピン留め処理部
     if payload.emoji.name == '📌':
         user = client.get_user(payload.user_id)
         if user.bot: return
@@ -43,6 +43,20 @@ async def on_raw_reaction_add(payload):
             if message.pinned == 0: 
                 await message.pin()
                 await message.channel.send(f"{user.name}がメッセージをピン留めしました。")
+
+# リアクション追加時の処理一覧
+@client.event
+async def on_raw_reaction_remove(payload):
+    # ピン解除処理部
+    if payload.emoji.name == '📌':
+        user = client.get_user(payload.user_id)
+        if user.bot: return
+        else: 
+            channel = client.get_channel(payload.channel_id)
+            message = await channel.fetch_message(payload.message_id)
+            if message.reaction.count == 0:
+                await message.unpin()
+                await message.channel.send(f"メッセージのピン留めを解除しました。\n>>> {message.content}")
 
 # Botの起動とDiscordサーバーへの接続処理部
 client.run('Njc4MDM0Mzc3OTc2MDUzNzYx.XkdcfA.wNgxL19wmcvvXIsysVOxWmNYDhE')
