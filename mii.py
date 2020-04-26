e969466d74acb1d4fd9833c7642e9772117d2f75
fe-amazu <fe-amazu@users.noreply.github.com>
2020-04-26 18:20:11 +0900

    コードが荒れていたので修正

--- mii.py
+++ mii.py
@@ -3,32 +3,30 @@ import os
 import discord
 import dotenv
 
-from discord import Message
-
 dotenv.load_dotenv()
 
 client = discord.Client()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
+
 # consts
-CH_STARTUP = int(os.getenv("CH_STARTUP",         "678483492564107284"))
-CH_REGISTER = int(os.getenv("CH_REGISTER",        "653111096747491328"))
-CH_JOIN = int(os.getenv("CH_JOIN",            "653923742245978129"))
-CH_ROOM_MASTER = int(os.getenv("CH_ROOM_MASTER",     "702042912338346114"))
-CH_THREAD_MASTER = int(os.getenv("CH_THREAD_MASTER",   "702030388033224714"))
-CH_VOICE = int(os.getenv("CH_VOICE",           "655319117691355166"))
-CH_VOICE_TEXT = int(os.getenv("CH_VOICE_TEXT",      "655319030428598303"))
+CH_STARTUP = int(os.getenv("CH_STARTUP", "678483492564107284"))
+CH_REGISTER = int(os.getenv("CH_REGISTER", "653111096747491328"))
+CH_JOIN = int(os.getenv("CH_JOIN", "653923742245978129"))
+CH_ROOM_MASTER = int(os.getenv("CH_ROOM_MASTER", "702042912338346114"))
+CH_THREAD_MASTER = int(os.getenv("CH_THREAD_MASTER", "702030388033224714"))
+CH_VOICE = int(os.getenv("CH_VOICE", "655319117691355166"))
+CH_VOICE_TEXT = int(os.getenv("CH_VOICE_TEXT", "655319030428598303"))
 
-CAT_ROOM = int(os.getenv("CAT_ROOM",           "702044270609170443"))
-CAT_THREAD = int(os.getenv("CAT_THREAD",         "662856289151615025"))
-CAT_ARCHIVE = int(os.getenv("CAT_ARCHIVE",        "702074011772911656"))
+CAT_ROOM = int(os.getenv("CAT_ROOM", "702044270609170443"))
+CAT_THREAD = int(os.getenv("CAT_THREAD", "662856289151615025"))
+CAT_ARCHIVE = int(os.getenv("CAT_ARCHIVE", "702074011772911656"))
 
-MEMBER_ROLE_NAME = str(os.getenv("MEMBER_ROLE_NAME",   "member"))
-ARCHIVE_ROLE_NAME = str(os.getenv("ARCHIVE_ROLE_NAME",  "view archive"))
+MEMBER_ROLE_NAME = str(os.getenv("MEMBER_ROLE_NAME", "member"))
+ARCHIVE_ROLE_NAME = str(os.getenv("ARCHIVE_ROLE_NAME", "view archive"))
+
 
 # functions
-
-
 async def register(message):
     if not message.channel.id == CH_REGISTER:
         await message.channel.send("ここでは実行できません。")
