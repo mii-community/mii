1173907c122e99b5eb55ccb18366d0e61acd313b
fe-amazu <fe-amazu@users.noreply.github.com>
2020-05-16 12:54:20 +0900

    ルーム機能あたりを修正

--- mii.py
+++ mii.py
@@ -8,7 +8,6 @@ dotenv.load_dotenv()
 client = discord.Client()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-
 # consts
 CH_STARTUP = int(os.getenv("CH_STARTUP", "678483492564107284"))
 CH_REGISTER = int(os.getenv("CH_REGISTER", "653111096747491328"))
@@ -20,7 +19,8 @@ CH_VOICE_TEXT = int(os.getenv("CH_VOICE_TEXT", "655319030428598303"))
 
 CAT_ROOM = int(os.getenv("CAT_ROOM", "702044270609170443"))
 CAT_THREAD = int(os.getenv("CAT_THREAD", "662856289151615025"))
-CAT_ARCHIVE = int(os.getenv("CAT_ARCHIVE", "702074011772911656"))
+CAT_THREAD_ARCHIVE = int(os.getenv("CAT_THREAD_ARCHIVE", "702074011772911656"))
+CAT_ROOM_ARCHIVE = int(os.getenv("CAT_THREAD_ARCHIVE", "711058666387800135"))
 
 MEMBER_ROLE_NAME = str(os.getenv("MEMBER_ROLE_NAME", "member"))
 ARCHIVE_ROLE_NAME = str(os.getenv("ARCHIVE_ROLE_NAME", "view archive"))
@@ -105,7 +105,7 @@ async def register(message):
     )
 
 
-async def add_room(message):
+async def open_room(message):
     if message.channel.id != CH_ROOM_MASTER:
         await message.channel.send("ここでは実行できません。")
         return
@@ -114,6 +114,22 @@ async def add_room(message):
         if channel.topic == "room-author: " + str(message.author.id):
             await message.channel.send(
                 f"{message.author.mention} {channel.mention} あなたの部屋はもう作られています。"
+            )
+            return
+    category = client.get_channel(CAT_ROOM_ARCHIVE)
+    for channel in category.channels:
+        if channel.topic == "room-author: " + str(message.author.id):
+            await channel.edit(category=client.get_channel(CAT_R))
+            role = discord.utils.get(
+                message.guild.roles, name=ARCHIVE_ROLE_NAME)
+            await channel.set_permissions(role, overwrite=None)
+            role = discord.utils.get(
+                message.guild.roles, name=MEMBER_ROLE_NAME)
+            await channel.set_permissions(role, read_messages=True)
+            creator = channel.guild.get_member(message.author.id)
+            await channel.set_permissions(creator, manage_messages=True)
+            await message.channel.send(
+                f"{message.author.mention} {channel.mention} をアーカイブから戻しました。"
             )
             return
     named = message.author.display_name + "の部屋"
@@ -143,7 +159,7 @@ async def open_thread(message):
         await message.channel.send(
             f"{message.author.mention} {matched.mention} はもう作られています。"
         )
-    elif matched.category.id == CAT_ARCHIVE:
+    elif matched.category.id == CAT_THREAD_ARCHIVE:
         await matched.edit(category=client.get_channel(CAT_THREAD))
         role = discord.utils.get(message.guild.roles, name=ARCHIVE_ROLE_NAME)
         await matched.set_permissions(role, overwrite=None)
@@ -162,19 +178,27 @@ async def age_thread(message):
     await message.channel.edit(position=position)
 
 
-async def close_thread(message):
-    if message.channel.category.id != CAT_THREAD:
+async def close_ch(message):
+    if (message.channel.category.id != CAT_ROOM
+            and message.channel.category.id != CAT_THREAD):
         await message.channel.send("ここでは実行できません。")
         return
-    if (message.author.guild_permissions.administrator
-            or message.channel.topic == "thread-author: " + str(message.author.id)):
+    elif (message.channel.topic != "room-author: " + str(message.author.id)
+            and message.channel.topic != "thread-author: " + str(message.author.id)):
+        await message.channel.send("権限がありません。")
+        return
+    elif message.channel.category.id == CAT_ROOM:
         role = discord.utils.get(message.guild.roles, name=MEMBER_ROLE_NAME)
         await message.channel.set_permissions(role, overwrite=None)
         role = discord.utils.get(message.guild.roles, name=ARCHIVE_ROLE_NAME)
         await message.channel.set_permissions(role, read_messages=True, send_messages=False)
-        await message.channel.edit(category=client.get_channel(CAT_ARCHIVE))
-    else:
-        await message.channel.send("権限がありません。")
+        await message.channel.edit(category=client.get_channel(CAT_ROOM_ARCHIVE))
+    elif message.channel.category.id == CAT_THREAD:
+        role = discord.utils.get(message.guild.roles, name=MEMBER_ROLE_NAME)
+        await message.channel.set_permissions(role, overwrite=None)
+        role = discord.utils.get(message.guild.roles, name=ARCHIVE_ROLE_NAME)
+        await message.channel.set_permissions(role, read_messages=True, send_messages=False)
+        await message.channel.edit(category=client.get_channel(CAT_THREAD_ARCHIVE))
 
 
 async def rename_ch(message):
@@ -353,24 +377,24 @@ async def on_message(message):
         return
     elif message.content == "!register":
         await register(message)
-    elif message.content.startswith("!vc "):
-        await rename_vc(message)
     elif message.content == "!open":
-        await add_room(message)
+        await open_room(message)
     elif message.content.startswith("!open "):
         await open_thread(message)
-    elif message.content == "!close":
-        await close_thread(message)
     elif message.content.startswith("!rename "):
         await rename_ch(message)
+    elif message.content == "!close":
+        await close_ch(message)
+    elif message.content.startswith("!vc "):
+        await rename_vc(message)
     elif message.content.startswith("!purge "):
         await purge(message)
+    elif message.content.startswith("!emoji "):
+        await replace_emoji(message)
     elif message.content == "!help":
         await send_help(message)
-    elif message.channel.category.id == CAT_THREAD:
+    if message.channel.category.id == CAT_THREAD:
         await age_thread(message)
-    if message.content.startswith("!emoji "):
-        await replace_emoji(message)
 
 
 @client.event
