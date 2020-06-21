3f7ed7f9f1eee0dc0630ef47b4f0a16a1626c6fd
fe-amazu <tenzyu.on@gmail.com>
2020-06-21 16:12:55 +0900

    mii.pyのage_thread()を移植

--- cogs/thread.py
+++ cogs/thread.py
@@ -18,36 +18,38 @@ class ThreadCog(commands.Cog):
     async def on_message(self, message):
         if message.guild.id != 724131075764387971:
             return
-
+        
         if (message.author.bot
-            or message.channel.id != CH_THREAD_MASTER):
+            or message.channel.category.id != CAT_THREAD):
             return
-
-        named = message.content
-        matched = discord.utils.get(message.guild.channels, name=named)
-        if not matched:
-            new_channel = await self.bot.get_channel(CAT_THREAD).create_text_channel(name=named)
-            await new_channel.edit(topic="thread-author: " + str(message.author.id))
-            await message.channel.send(
-                f"{message.author.mention} {new_channel.mention} を作成しました。"
-            )
-        elif matched.category.id == CAT_THREAD:
-            await message.channel.send(
-                f"{message.author.mention} {matched.mention} はもう作られています。"
-            )
-        elif matched.category.id == CAT_THREAD_ARCHIVE:
-            await matched.edit(category=self.bot.get_channel(CAT_THREAD))
-            role = discord.utils.get(
-                message.guild.roles, name=ARCHIVE_ROLE_NAME)
-            await matched.set_permissions(role, overwrite=None)
-            role = discord.utils.get(
-                message.guild.roles, name=MEMBER_ROLE_NAME)
-            await matched.set_permissions(role, read_messages=True)
-            await matched.edit(topic="thread-author: " + str(message.author.id))
-            await message.channel.send(
-                f"{message.author.mention} {matched.mention} をアーカイブから戻しました。スレッドの作者は上書きされました。"
-            )
-
+        elif message.channel.id == CH_THREAD_MASTER:
+            named = message.content
+            matched = discord.utils.get(message.guild.channels, name=named)
+            if not matched:
+                new_channel = await self.bot.get_channel(CAT_THREAD).create_text_channel(name=named)
+                await new_channel.edit(topic="thread-author: " + str(message.author.id))
+                await message.channel.send(
+                    f"{message.author.mention} {new_channel.mention} を作成しました。"
+                )
+            elif matched.category.id == CAT_THREAD:
+                await message.channel.send(
+                    f"{message.author.mention} {matched.mention} はもう作られています。"
+                )
+            elif matched.category.id == CAT_THREAD_ARCHIVE:
+                await matched.edit(category=self.bot.get_channel(CAT_THREAD))
+                role = discord.utils.get(
+                    message.guild.roles, name=ARCHIVE_ROLE_NAME)
+                await matched.set_permissions(role, overwrite=None)
+                role = discord.utils.get(
+                    message.guild.roles, name=MEMBER_ROLE_NAME)
+                await matched.set_permissions(role, read_messages=True)
+                await matched.edit(topic="thread-author: " + str(message.author.id))
+                await message.channel.send(
+                    f"{message.author.mention} {matched.mention} をアーカイブから戻しました。スレッドの作者は上書きされました。"
+                )
+        else:
+            position = self.bot.get_channel(CH_THREAD_MASTER).position + 1
+            await message.channel.edit(position=position)
 
 def setup(bot):
     bot.add_cog(ThreadCog(bot))
