2db729be31f85c0bdceba0674d2ce42b42e881a2
fe-amazu <tenzyu.on@gmail.com>
2020-06-21 17:55:50 +0900

    mii.pyからrename_chを移植

---
+++ cogs/rename_ch.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import os
+
+# consts
+CAT_ROOM = int(os.getenv("CAT_ROOM", "702044270609170443"))
+CAT_THREAD = int(os.getenv("CAT_THREAD", "662856289151615025"))
+
+
+class Rename_chCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rename(self, ctx, named):
+        if (ctx.channel.category.id != CAT_ROOM
+                and ctx.channel.category.id != CAT_THREAD):
+            await ctx.send("ここでは実行できません。")
+            return
+        elif (ctx.channel.topic != "room-author: " + str(ctx.author.id)
+                and ctx.channel.topic != "thread-author: " + str(ctx.author.id)
+                and (not ctx.author.guild_permissions.administrator)):
+            await ctx.send("権限がありません。")
+            return
+        await ctx.channel.edit(name=named)
+        await ctx.send(f"{ctx.author.mention} チャンネル名を {named} に上書きしました。")
+
+
+def setup(bot):
+    bot.add_cog(Rename_chCog(bot))
