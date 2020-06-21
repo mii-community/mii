c06502e37afcaf8993b13cc427e8b9d278e9198f
fe-amazu <tenzyu.on@gmail.com>
2020-06-21 14:50:48 +0900

    コグを読み込むようにした

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+
+from discord.ext import commands
+
 import os
+import traceback
 
 import dotenv
 dotenv.load_dotenv()
 
-from discord.ext import commands
+
+EXTENSIONS = [
+    "cogs.room"
+]
+
 
 class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or("!"))
+        
+        for cog in EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
 
     async def on_ready(self):
-        print("logged in:" + self.user.name, self.user.id)
+        print("logged in: ", self.user.name, self.user.id)
+
 
 if __name__ == '__main__':
-    bot = MyBot(command_prefix='!')
+    bot = MyBot()
     bot.run(os.getenv("DISCORD_BOT_TOKEN"))
