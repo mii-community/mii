d59ef9b6b1fce084c751ae147dfdbf4d5ae2b940
fe-amazu <tenzyu.on@gmail.com>
2020-06-21 18:09:53 +0900

    mii.pyからpurgeを移植

---
+++ cogs/purge.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+
+class PurgeCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def purge(self, ctx, num):
+        channel = ctx.channel
+        if not ctx.author.permissions_in(channel).manage_messages:
+            await channel.send(f"メッセージ管理の権限がありません。")
+            return
+        if num == "all":
+            await channel.purge(limit=None)
+            await ctx.send("✅")
+            return
+        try:
+            num = int(num)
+        except:
+            await channel.send(
+                f"不正な引数です。削除するメッセージ数か、全て削除する場合はallを入力してください。"
+            )
+            return
+        await channel.purge(limit=num)
+        await ctx.send("✅")
+
+
+def setup(bot):
+    bot.add_cog(PurgeCog(bot))
