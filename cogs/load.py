847aa1a7947a4b64786659aab2c448ee30dbb938
fe-amazu <tenzyu.on@gmail.com>
2020-06-25 21:27:06 +0900

    ロードコマンド作成

--- cogs/load.py
+++ cogs/load.py
@@ -17,6 +17,15 @@ class Load(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, cog):
+        try:
+            self.bot.load_extension("cogs." + cog)
+            await ctx.send(f"{cog}.pyは正常にリロードされました。")
+        except:
+            await ctx.send("リロードできませんでした。ログを確認してください。")
+            traceback.print_exc()
 
     @commands.command()
     @commands.is_owner()
