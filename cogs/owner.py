273e624b7921c4d0d04d78427a60d10cad93aa1b
fe-amazu <tenzyu.on@gmail.com>
2020-06-25 22:43:43 +0900

    dbのroom_idを上書きできるコマンドを作成

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,11 +1,30 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
 
 
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name="set")
+    @commands.is_owner()
+    async def db_set_room_id(self, ctx, member: discord.Member):
+        await self.bot.datebase.execute(
+            """
+            UPDATE mii
+            SET room_id = $1
+            WHERE user_id = $2
+            AND guild_id = $3
+            """,
+            ctx.channel.id, member.id, ctx.guild.id
+        )
+        await ctx.send(f"このチャンネルの所有者は{member.display_name}にセットされました。")
+
+        if not member.permissions_in(ctx.channel).manage_messages:
+            await ctx.channel.set_permissions(member, manage_messages=True)
+
 
 def setup(bot):
     bot.add_cog(Owner(bot))
