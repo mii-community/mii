dde1341f802422f3a50fc5d101ed600a374d4a3c
fe-amazu <tenzyu.on@gmail.com>
2020-08-05 17:16:09 +0900

    refactored load.py

--- cogs/load.py
+++ cogs/load.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import glob
 
 
 class Load(commands.Cog, command_attrs=dict(hidden=True)):
@@ -9,12 +10,12 @@ class Load(commands.Cog, command_attrs=dict(hidden=True)):
 
     async def cog_check(self, ctx):
         if not await ctx.bot.is_owner(ctx.author):
-            ctx.send("権限がありません。")
+            await ctx.send("権限がありません。")
             return False
         return True
 
-    @commands.command()
-    async def load(self, ctx, cog):
+    @commands.command(name="load")
+    async def load_cog(self, ctx, cog):
         try:
             self.bot.load_extension("cogs." + cog)
             await ctx.send(f"{cog}.pyは正常にロードされました。")
@@ -22,8 +23,8 @@ class Load(commands.Cog, command_attrs=dict(hidden=True)):
             await ctx.send(f"{cog}.pyはロードできませんでした。ログを確認してください。")
             traceback.print_exc()
 
-    @commands.command()
-    async def unload(self, ctx, cog):
+    @commands.command(name="unload")
+    async def unload_cog(self, ctx, cog):
         try:
             self.bot.unload_extension("cogs." + cog)
             await ctx.send(f"{cog}.pyは正常にアンロードされました。")
@@ -31,23 +32,23 @@ class Load(commands.Cog, command_attrs=dict(hidden=True)):
             await ctx.send(f"{cog}.pyはアンロードできませんでした。ログを確認してください。")
             traceback.print_exc()
 
+    @commands.command(name="reload")
     async def reload_cog(self, ctx, cog):
         try:
-            cog = cog.replace('.py', '')
             self.bot.reload_extension("cogs." + cog)
-            await ctx.send(f"{cog}.pyは正常にリロードされました。")
+            await ctx.send(f"{cog}は正常にリロードされました。")
         except:
             await ctx.send(f"{cog}.pyはリロードできませんでした。ログを確認してください。")
-            traceback.print_exc()
 
-    @commands.command()
-    async def reload(self, ctx, cog):
-        if cog == "all":
-            for cog in [cogs for cogs in os.listdir("./cogs") if cogs.endswith(".py")]:
-                await self.reload_cog(ctx, cog)
-            await ctx.send("全て終わりました。")
-            return
-        await self.reload_cog(ctx, cog)
+    @commands.command(name="reloadall")
+    async def reload_all_cogs(self, ctx):
+        for cog in [cog.replace("/", ".").replace(".py", "") for cog in glob.glob("cogs/*.py")]:
+            try:
+                self.bot.reload_extension(cog)
+                await ctx.send(f"{cog}は正常に読み込まれました。")
+            except:
+                traceback.print_exc()
+        await ctx.send("すべて終わりました。")
 
 
 def setup(bot):
