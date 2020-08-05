e257da756a97ef23c3c4016ad4f7c32da0ff1987
fe-amazu <tenzyu.on@gmail.com>
2020-08-05 18:51:10 +0900

    early return

--- cogs/register.py
+++ cogs/register.py
@@ -9,20 +9,21 @@ class RegisterCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, reaction_event):
-        if reaction_event.channel_id == constant.CH_REGISTER:
-            channel = self.bot.get_channel(constant.CH_REGISTER)
-            guild = self.bot.get_guild(reaction_event.guild_id)
-            message = await channel.fetch_message(reaction_event.message_id)
-            member = reaction_event.member
-            role = guild.get_role(constant.ROLE_MEMBER)
-            if role in member.roles:
-                await message.remove_reaction(reaction_event.emoji, member)
-                return
+        if reaction_event.channel_id != constant.CH_REGISTER:
+            return
+        channel = self.bot.get_channel(constant.CH_REGISTER)
+        guild = self.bot.get_guild(reaction_event.guild_id)
+        message = await channel.fetch_message(reaction_event.message_id)
+        member = reaction_event.member
+        role = guild.get_role(constant.ROLE_MEMBER)
+        if role in member.roles:
             await message.remove_reaction(reaction_event.emoji, member)
-            await member.add_roles(role)
-            await self.bot.get_channel(constant.CH_JOIN).send(
-                f"{reaction_event.member.mention}が参加しました。"
-            )
+            return
+        await message.remove_reaction(reaction_event.emoji, member)
+        await member.add_roles(role)
+        await self.bot.get_channel(constant.CH_JOIN).send(
+            f"{reaction_event.member.mention}が参加しました。"
+        )
 
 
 def setup(bot):
