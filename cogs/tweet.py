6313dafabb5f9348595c57dbc379d453221ebe71
fe-amazu <tenzyu.on@gmail.com>
2020-08-20 01:36:10 +0900

    created tweet.py

--- cogs/tweet.py
+++ cogs/tweet.py
@@ -10,8 +10,28 @@ class TweetCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # つぶやきchのログを、古いものから、指定件数分までだけが残るように削除
         if message.channel.id != launcher.CH_TWEET:
             return
+
+        # メッセージ数(num)を取得
+        num = 0
+        async for msg in message.channel.history():
+            num += 1
+
+        # 残すメッセージ数を指定
+        log = 15
+
+        # 指定件数以下ならば無視
+        if num < log:
+            return
+
+        # BOT停止などで複数件更新されていた場合やerror:unknown messageなどの対策も含めてリスト化してから削除
+        arrayMsg = []
+        async for msg in message.channel.history(limit=num - log, oldest_first=True):
+            arrayMsg.append(msg)
+        await self.bot.get_channel(launcher.CH_TWEET).delete_messages(arrayMsg)
+
 
 def setup(bot):
     bot.add_cog(TweetCog(bot))
