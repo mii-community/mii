7cd0837f9232c89d15b91b7eaae1c67f8cb237c2
chun37 <chun.jb.37@gmail.com>
2020-09-29 09:46:30 +0900

    少しは読みやすくなったはず

--- cogs/register.py
+++ cogs/register.py
@@ -14,16 +14,19 @@ class RegisterCog(commands.Cog):
     async def on_raw_reaction_add(self, reaction_event):
         if reaction_event.channel_id != constant.CH_REGISTER:
             return
+
         channel = self.bot.get_channel(constant.CH_REGISTER)
         guild = self.bot.get_guild(reaction_event.guild_id)
         message = await channel.fetch_message(reaction_event.message_id)
-        member = reaction_event.member
-        role = guild.get_role(constant.ROLE_MEMBER)
-        if role in member.roles:
-            await message.remove_reaction(reaction_event.emoji, member)
+
+        new_member = reaction_event.member
+        member_role = guild.get_role(constant.ROLE_MEMBER)
+
+        await message.remove_reaction(reaction_event.emoji, new_member)
+        if member_role in new_member.roles:
             return
-        await message.remove_reaction(reaction_event.emoji, member)
-        await member.add_roles(role)
+
+        await new_member.add_roles(member_role)
         await self.bot.get_channel(constant.CH_JOIN).send(
             f"{reaction_event.member.mention}が参加しました。"
         )
