2e36adb7896b2e86101f1f1adae7bbc26e04c396
tenzyu <tenzyu.on@gmail.com>
2021-02-01 21:03:37 +0900

    better purge

--- cogs/purge.py
+++ cogs/purge.py
@@ -1,31 +1,30 @@
-from discord.ext import commands
+from discord.ext.commands import Bot, Cog, Context, command
 
 
-class PurgeCog(commands.Cog):
-    def __init__(self, bot):
+class Purge(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    async def purging(self, ctx, limit):
+    async def purging(self, ctx: Context, limit):
         if not ctx.author.permissions_in(ctx.channel).manage_messages:
             await ctx.send("メッセージ管理の権限がありません。")
             return
         await ctx.channel.purge(limit=limit)
-        await ctx.send("✅")
+        await ctx.send("✅", delete_after=10)
 
-    @commands.command(name="purge")
-    async def purge_messages(self, ctx, num):
-        """!purge <number> で指定された数のメッセージを一括削除します。"""
+    @command(name="purge")
+    async def purge_messages(self, ctx: Context, num):
+        """!purge <num> で指定された数のメッセージを一括削除します。"""
         if not num.isdecimal():
             await ctx.send("数値を入力してください。")
             return
-        num = int(num)
-        await self.purging(ctx, num + 1)
+        await self.purging(ctx, int(num) + 1)
 
-    @commands.command(name="purgeall")
-    async def purge_all_messages(self, ctx):
+    @command(name="purgeall")
+    async def purge_all_messages(self, ctx: Context):
         """全てのメッセージを一括削除します。"""
         await self.purging(ctx, None)
 
 
-def setup(bot):
-    bot.add_cog(PurgeCog(bot))
+def setup(bot: Bot):
+    bot.add_cog(Purge(bot))
