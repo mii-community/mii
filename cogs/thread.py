775e657edff589f2b581414e20b959a87998ecee
tenzyu <tenzyu.on@gmail.com>
2021-02-01 19:47:41 +0900

    refactored thread.py

--- cogs/thread.py
+++ cogs/thread.py
@@ -1,13 +1,10 @@
-import os
-
-import discord
-from discord.ext import commands
-
 import constant
+from discord import Message, utils
+from discord.ext.commands import Bot, Cog
 
 
-class ThreadCog(commands.Cog):
-    def __init__(self, bot):
+class Thread(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
     async def set_admin(self, author_id, channel_id):
@@ -28,47 +25,44 @@ class ThreadCog(commands.Cog):
         )
         return
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot or message.channel.category.id != constant.CAT_THREAD:
+    @Cog.listener()
+    async def on_message(self, message: Message):
+        author = message.author
+        channel = message.channel
+        if author.bot or channel.category.id != constant.CAT_THREAD:
             return
 
-        elif message.channel.id != constant.CH_THREAD_MASTER:
-            position = self.bot.get_channel(
-                constant.CH_THREAD_MASTER).position + 1
-            await message.channel.edit(position=position)
+        # 新着順ソート
+        if channel.id != constant.CH_THREAD_MASTER:
+            position = self.bot.get_channel(constant.CH_THREAD_MASTER).position + 1
+            await channel.edit(position=position)
             return
-        ch_main = self.bot.get_channel(constant.CH_MAIN)
+
         # 簡易的なCH名の重複チェック
         name = message.content
-        ch_thread = discord.utils.get(message.guild.channels, name=name)
+        guild = message.guild
+        ch_thread = utils.get(guild.channels, name=name)
         cat_thread = self.bot.get_channel(constant.CAT_THREAD)
+        ch_main = self.bot.get_channel(constant.CH_MAIN)
         # 同名CHがない場合
-        if not ch_thread:
+        if ch_thread is None:
             new_thread = await cat_thread.create_text_channel(name=name)
-            await message.channel.send(
-                f"{message.author.mention} {new_thread.mention} を作成しました。"
-            )
-            await self.set_admin(message.author.id, new_thread.id)
-            await ch_main.send(f"新しいスレッドが開始されました。{new_thread.mention}")
+            await channel.send(f"{author.mention} {new_thread.mention} を作成しました。")
+            await ch_main.send(f"{new_thread.mention} が作成されました。")
+            await self.set_admin(author.id, new_thread.id)
             return
-
         # 同名CHがスレッドカテゴリーにある場合
         if ch_thread.category.id == constant.CAT_THREAD:
             text = "はもう作られています。"
         # 同名CHがアーカイブカテゴリーにある場合
         elif ch_thread.category.id == constant.CAT_THREAD_ARCHIVE:
             text = "をアーカイブから戻しました。"
-            role_archive = message.guild.get_role(constant.ROLE_ARCHIVE)
-            role_member = message.guild.get_role(constant.ROLE_MEMBER)
             await ch_thread.edit(category=cat_thread)
             await ch_thread.edit(sync_permissions=True)
-            await self.update_admin(message.author.id, ch_thread.id)
-            await ch_main.send(f"スレッドが再開されました。{ch_thread.mention}")
-        # おわりに
-        await message.channel.send(
-            f"{message.author.mention} {ch_thread.mention} {text}"
-        )
+            await ch_main.send(f"{ch_thread.mention} が再開されました。")
+            await self.update_admin(author.id, ch_thread.id)
+        await channel.send(f"{author.mention} {ch_thread.mention} {text}")
 
-def setup(bot):
-    bot.add_cog(ThreadCog(bot))
+
+def setup(bot: Bot):
+    bot.add_cog(Thread(bot))
