1e471131c8189fea51c37aa91ad3396bee68f9f7
tenzyu <tenzyu.on@gmail.com>
2021-02-01 20:16:14 +0900

    refactored room.py

--- cogs/room.py
+++ cogs/room.py
@@ -1,63 +1,56 @@
-import os
-
-import discord
-from discord.ext import commands
-
 import constant
+from discord.ext.commands import Bot, Cog, Context, command
 
 
-class Room(commands.Cog):
-    def __init__(self, bot):
+class Room(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command()
-    async def open(self, ctx):
+    @command()
+    async def open(self, ctx: Context):
         """ルームマスターで使うことであなたの部屋を作成します。"""
-        if ctx.author.bot:
+        author = ctx.author
+        if author.bot:
             return
-        elif ctx.channel.id != constant.CH_ROOM_MASTER:
+        channel = ctx.channel
+        if channel.id != constant.CH_ROOM_MASTER:
             await ctx.send("ここでは実行できません。")
             return
-
-        ch_data = await self.bot.database.fetch_row(
-            constant.TABLE_NAME, author_id=ctx.author.id, channel_type="room"
+        data_room = await self.bot.database.fetch_row(
+            constant.TABLE_NAME, author_id=author.id, channel_type="room"
         )
-
-        creator = ctx.guild.get_member(ctx.author.id)
+        guild = ctx.guild
+        creator = guild.get_member(author.id)
         cat_room = self.bot.get_channel(constant.CAT_ROOM)
         # ルームを持っていない場合
-        if not ch_data:
-            name = f"{ctx.author.display_name}の部屋"
+        if data_room is None:
+            name = f"{author.display_name}の部屋"
             new_room = await cat_room.create_text_channel(name=name)
             await new_room.set_permissions(
                 creator, manage_messages=True, manage_channels=True
             )
-            await ctx.send(f"{ctx.author.mention} {new_room.mention} を作成しました。")
+            await ctx.send(f"{author.mention} {new_room.mention} を作成しました。")
             await self.bot.database.insert(
                 constant.TABLE_NAME,
                 channel_id=new_room.id,
-                author_id=ctx.author.id,
+                author_id=author.id,
                 channel_type="room",
             )
             return
-
-        ch_room = self.bot.get_channel(ch_data["channel_id"])
-        cat_room_archive = self.bot.get_channel(constant.CAT_ROOM_ARCHIVE)
         # ルームカテゴリーにある場合
+        ch_room = self.bot.get_channel(data_room["channel_id"])
         if ch_room.category == cat_room:
             text = "あなたの部屋はもう作られています。"
         # アーカイブカテゴリーにある場合
-        elif ch_room.category == cat_room_archive:
+        elif ch_room.category.id == constant.CAT_ROOM_ARCHIVE:
             text = "をアーカイブから戻しました。"
-            role_member = ctx.guild.get_role(constant.ROLE_MEMBER)
             await ch_room.edit(category=cat_room)
             await ch_room.edit(sync_permissions=True)
             await ch_room.set_permissions(
                 creator, manage_messages=True, manage_channels=True
             )
-        # おわりに
-        await ctx.send(f"{ctx.author.mention} {ch_room.mention} {text}")
+        await ctx.send(f"{author.mention} {ch_room.mention} {text}")
 
 
-def setup(bot):
+def setup(bot: Bot):
     bot.add_cog(Room(bot))
