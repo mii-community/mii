0c17581422d8bffd540c5fbbe522c6478fff814c
tenzyu <tenzyu.on@gmail.com>
2021-02-01 21:12:08 +0900

    rewrite poll

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,15 +1,16 @@
-import discord
-from discord.ext import commands
+from discord import Embed
+from discord.ext.commands import Bot, Cog, Context, command
 
 
-class Poll(commands.Cog):
-    def __init__(self, bot):
+class Poll(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command()
-    async def poll(self, ctx, title, *args):
+    @command()
+    async def poll(self, ctx: Context, title: str, *args):
         """!poll <title> <任意の数の要素(21未満、指定しなければ さんせいorはんたい)> で投票を行います。"""
         if len(args) > 21:
+            await ctx.send("引数が多すぎます。")
             return
         emoji = 0x0001F1E6
         num = 0
@@ -26,10 +27,10 @@ class Poll(commands.Cog):
                 content += f"{reac}：{arg}\n"
                 num += 1
                 emojis.append(reac)
-        embed = discord.Embed(title=title, description=content, color=0x3AEE67)
+        embed = Embed(title=title, description=content, color=0x3AEE67)
         msg = await ctx.send(embed=embed)
         [await msg.add_reaction(e) for e in emojis]
 
 
-def setup(bot):
+def setup(bot: Bot):
     bot.add_cog(Poll(bot))
