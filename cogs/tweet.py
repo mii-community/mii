972625d10c3d6e41d2d57be01f6573315255aa41
tenzyu <tenzyu.on@gmail.com>
2021-02-01 17:41:40 +0900

    refactored tweet.py

--- cogs/tweet.py
+++ cogs/tweet.py
@@ -1,33 +1,19 @@
-import os
-
-import discord
-from discord.ext import commands
-
 import constant
+from discord import Message
+from discord.ext.commands import Bot, Cog
 
 
-class TweetCog(commands.Cog):
-    def __init__(self, bot):
+class Tweet(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        # つぶやきchのログを、古いものから、指定件数分までだけが残るように削除
+    @Cog.listener()
+    async def on_message(self, message: Message):
         if message.channel.id != constant.CH_TWEET:
             return
-
         messages = await message.channel.history().flatten()
-
-        # 残すメッセージ数を指定
-        limit = 15
-
-        # 指定件数以下ならば無視
-        delete_messages = messages[limit:]
-
-        # BOT停止などで複数件更新されていた場合やerror:unknown messageなどの対策も含めてリスト化してから削除
-
-        await self.bot.get_channel(constant.CH_TWEET).delete_messages(delete_messages)
+        await message.channel.delete_messages(messages[15:])
 
 
-def setup(bot):
-    bot.add_cog(TweetCog(bot))
+def setup(bot: Bot):
+    bot.add_cog(Tweet(bot))
