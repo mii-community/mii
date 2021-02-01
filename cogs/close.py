5373104197899c5c30923f2f7ff11f5eb2d4d74d
tenzyu <tenzyu.on@gmail.com>
2021-02-01 21:40:43 +0900

    better code close.py

--- cogs/close.py
+++ cogs/close.py
@@ -1,47 +1,41 @@
-from discord.ext import commands
-
 import constant
+from discord.ext.commands import Bot, Cog, Context, command
 
 
-class CloseCog(commands.Cog):
-    def __init__(self, bot):
+class Close(Cog):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command()
-    async def close(self, ctx):
+    @command()
+    async def close(self, ctx: Context):
         """自分の作成した部屋/スレッドをアーカイブします。"""
-        if ctx.author.bot:
+        author = ctx.author
+        if author.bot:
             return
-        elif ctx.channel.category.id not in (constant.CAT_ROOM, constant.CAT_THREAD):
+        channel = ctx.channel
+        if not channel.category.id in (constant.CAT_ROOM, constant.CAT_THREAD):
             await ctx.send("ここでは実行できません。")
             return
-
-        ch_data = await self.bot.database.fetch_row(
-            constant.TABLE_NAME, channel_id=ctx.channel.id
+        data_ch = await self.bot.database.fetch_row(
+            constant.TABLE_NAME, channel_id=channel.id
         )
-
-        if not ch_data:
+        if data_ch is None:
             await ctx.send("データが存在しませんでした。")
             return
-        elif ctx.author.id != ch_data["author_id"]:
+        elif not (
+            author.id == data_ch["author_id"]
+            or author.permissions_in(channel).administrator
+        ):
             await ctx.send("権限がありません。")
             return
 
-        if ctx.channel.category.id == constant.CAT_ROOM:
+        if channel.category.id == constant.CAT_ROOM:
             goto_cat = self.bot.get_channel(constant.CAT_ROOM_ARCHIVE)
-        elif ctx.channel.category.id == constant.CAT_THREAD:
+        elif channel.category.id == constant.CAT_THREAD:
             goto_cat = self.bot.get_channel(constant.CAT_THREAD_ARCHIVE)
-        await ctx.channel.edit(category=goto_cat)
-
-        # role_member = ctx.guild.get_role(constant.ROLE_MEMBER)
-        # role_archive = ctx.guild.get_role(constant.ROLE_ARCHIVE)
-        # await ctx.channel.set_permissions(role_member, overwrite=None)
-        # await ctx.channel.set_permissions(
-        #     role_archive, read_messages=True, send_messages=False
-        # )
-
-        await ctx.channel.edit(sync_permissions=True)
+        await channel.edit(category=goto_cat)
+        await channel.edit(sync_permissions=True)
 
 
-def setup(bot):
-    bot.add_cog(CloseCog(bot))
+def setup(bot: Bot):
+    bot.add_cog(Close(bot))
