c1d2bbc12298d01e8df01a5557ca1025f8697d71
tenzyu <tenzyu.on@gmail.com>
2021-02-16 21:42:50 +0900

    ヌオーの定義場所を外にした。

--- cogs/nuoh.py
+++ cogs/nuoh.py
@@ -3,13 +3,14 @@ from discord.ext.commands import Bot, Cog
 
 
 class Nuoh(Cog):
+    nuoh = ("ぬおー", "ヌオー", "nuoh", "nuo-")
+
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @Cog.listener()
     async def on_message(self, message: Message):
-        nuoh = ("ぬおー", "ヌオー", "nuoh", "nuo-")
-        if not any(map(message.content.__contains__, nuoh)):
+        if not any(map(message.content.__contains__, self.nuoh)):
             return
         await message.channel.send(
             "https://cdn.discordapp.com/attachments/752286472383758416/807988458290806854/unknown.png"
