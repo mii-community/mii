e1c2acf665a63b992ac9e0355349b0518730eb72
tenzyu <tenzyu.on@gmail.com>
2021-02-17 07:43:06 +0900

    updated nuoh

--- cogs/nuoh.py
+++ cogs/nuoh.py
@@ -4,17 +4,23 @@ from discord.ext.commands import Bot, Cog
 
 class Nuoh(Cog):
     nuoh = ("ぬおー", "ヌオー", "nuoh", "nuo-")
+    nuoh_king = ("ぬ王", "ヌ王", "ぬ・王", "ヌ・王")
 
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @Cog.listener()
     async def on_message(self, message: Message):
-        if not any(map(message.content.__contains__, self.nuoh)):
+        if any(map(message.content.__contains__, self.nuoh)):
+            await message.channel.send(
+                "https://cdn.discordapp.com/attachments/752286472383758416/807988458290806854/unknown.png"
+            )
             return
-        await message.channel.send(
-            "https://cdn.discordapp.com/attachments/752286472383758416/807988458290806854/unknown.png"
-        )
+        elif any(map(message.content.__contains__, self.nuoh_king)):
+            await message.channel.send(
+                "https://cdn.discordapp.com/attachments/752286472383758416/811338296151638016/image0.png"
+            )
+            return
 
 
 def setup(bot: Bot):
