09963145d11566d981c9f3fe19bf593ef1769037
tenzyu <tenzyu.on@gmail.com>
2021-02-19 07:31:33 +0900

    added pick command

---
+++ cogs/pick.py
@@ -0,0 +1,17 @@
+from random import choice
+
+from discord.ext.commands import Bot, Cog, Context, command
+
+
+class Pick(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @command(aliases=["p", "random", "choice", "dice"])
+    async def pick(self, ctx: Context, *args):
+        result = choice(args)
+        await ctx.send(result)
+
+
+def setup(bot: Bot):
+    bot.add_cog(Pick(bot))
