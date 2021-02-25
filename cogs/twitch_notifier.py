3679dd27940dee2d089b5a2914103a0bc976569b
tenzyu <tenzyu.on@gmail.com>
2021-02-25 11:02:31 +0900

    ユーザーのアクティビティに Streaming があれば通知するようにした。

---
+++ cogs/twitch_notifier.py
@@ -0,0 +1,32 @@
+import constant
+from discord import AllowedMentions, Embed, Member, Streaming
+from discord.ext.commands import Bot, Cog
+
+
+class TwitchNotifier(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_member_update(self, before: Member, after: Member):
+        if after.activities == before.activities:
+            return
+        activities = after.activities
+        if activities is None:
+            return
+        for activity in activities:
+            if not isinstance(activity, Streaming):
+                return
+            stream = activity
+            embed = Embed(title=stream.name, url=stream.url)
+            embed.add_field(name="Game:", value=stream.game, inline=True)
+            embed.set_author(
+                name=stream.twitch_name, url=stream.url, icon_url=after.avatar_url
+            )
+            embed.set_thumbnail(url=after.avatar_url)
+            msg = f"{after.name.mention} が {stream.platform} で配信を始めました！"
+            await self.bot.get_channel(constant.CH_TWITCH).send(msg, embed=embed, allowed_mentions=AllowedMentions.none())
+
+
+def setup(bot: Bot):
+    bot.add_cog(TwitchNotifier(bot))
