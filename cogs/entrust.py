bf1ff585a72ec27bb533009973923816bccd48a9
tenzyu <tenzyu.on@gmail.com>
2021-03-06 02:50:39 +0900

    no mentions

--- cogs/entrust.py
+++ cogs/entrust.py
@@ -1,5 +1,6 @@
 from random import choice, sample
 
+from discord import AllowedMentions
 from discord.ext.commands import Bot, Cog, Context, command
 
 
@@ -10,12 +11,12 @@ class Entrust(Cog):
     @command(aliases=["p", "random", "choice", "dice"])
     async def pick(self, ctx: Context, *args):
         result = choice(args)
-        await ctx.send(result)
+        await ctx.send(result, allowed_mentions=AllowedMentions.none())
 
     @command(aliases=["o"])
     async def order(self, ctx: Context, *args):
         result = " ".join(sample(args, len(args)))
-        await ctx.send(result)
+        await ctx.send(result, allowed_mentions=AllowedMentions.none())
 
 
 def setup(bot: Bot):
