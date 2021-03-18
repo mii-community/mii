86939e585ee3d5f51e0ade0055aea166872256e6
tenzyu <tenzyu.on@gmail.com>
2021-03-18 16:36:52 +0900

    use display_name instead of mention

--- cogs/vc_notifier.py
+++ cogs/vc_notifier.py
@@ -15,9 +15,13 @@ class VCNotifier(Cog):
         if not str(vc.id) in constant.VOICE_CHANNELS.keys():
             return
         if after.channel:
-            embed = Embed(description=f"{member.mention}が入室しました。", colour=0x000000)
+            embed = Embed(
+                description=f"{member.display_name} が入室しました。", colour=0x000000
+            )
         elif before.channel:
-            embed = Embed(description=f"{member.mention}が退室しました。", colour=0x000000)
+            embed = Embed(
+                description=f"{member.display_name} が退室しました。", colour=0x000000
+            )
         else:
             return
         embed.set_footer(text="このメッセージは30秒後に自動で削除されます。")
